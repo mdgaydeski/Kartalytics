35238e7ceb7a4ffe624536b5fe137281707d710b
mdgaydeski <mdgaydeski@gmail.com>
2021-06-03 13:12:27 -0500

    Install TypeScript

---
+++ Kartalytics/ClientApp/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <h1>Kartalytics</h1>
+            <p>Hello</p>
+        </>
+    );
+}
+
+export default App;
