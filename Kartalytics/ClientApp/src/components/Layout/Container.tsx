ced88ace555785f2f13fed245cd902242d0e6362
mdgaydeski <mdgaydeski@gmail.com>
2021-06-24 11:57:08 -0500

    Fix width on small viewport

--- Kartalytics/ClientApp/src/components/Layout/Container.tsx
+++ Kartalytics/ClientApp/src/components/Layout/Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Container: React.FC<Props> = ({ children }) => {
     return (
-        <div className='bg-black bg-opacity-90 border border-gray-600 my-8 mx-auto px-4 pb-4 rounded-xl shadow-rainbow w-3/4'>
+        <div className='bg-black bg-opacity-90 border border-gray-600 my-8 mx-auto px-4 pb-4 rounded-xl shadow-rainbow w-11/12 md:w-3/4'>
             {children}
         </div>
     ); 
