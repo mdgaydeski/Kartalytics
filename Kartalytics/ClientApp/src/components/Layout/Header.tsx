dc24590d5a96a45b79c9ae41c3804cfc052700a4
mdgaydeski <mdgaydeski@gmail.com>
2021-06-23 19:07:15 -0500

    Finish navbar layout

--- Kartalytics/ClientApp/src/components/Layout/Header.tsx
+++ Kartalytics/ClientApp/src/components/Layout/Header.tsx
@@ -1,21 +1,35 @@
 ﻿import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 import { MenuIcon } from '@heroicons/react/outline';
-import { SearchIcon } from '@heroicons/react/outline';
 import * as ROUTES from '../../constants/routes';
+import SearchBar from '../SearchBar';
+
+const { useState } = React;
 
 const Header = () => {
+    const linkClasses = 'block text-center py-3 w-full md:inline md:py-0 md:w-1/5';
+
+    const [navCollapsed, setNavCollapsed] = useState<boolean>(true);
+
     return (
         <header>
-            <nav className='flex bg-black bg-opacity-90'>
-                <NavLink to={ROUTES.HOME} className='font-display ml-3 text-2xl w-1/4'>Kartalytics</NavLink>
-                <MenuIcon className='h-6 w-6 my-auto ml-auto mr-3 border border-gray-400 rounded-md cursor-pointer md:hidden' />
-                <div className='hidden md:flex md:w-3/4'>
-                    <NavLink to={ROUTES.PLAYER_LIST} className='my-auto text-center w-1/4'>Players</NavLink>
-                    <NavLink to={ROUTES.TOURNAMENT_LIST} className='my-auto text-center w-1/4'>Tournaments</NavLink>
-                    <NavLink to={ROUTES.TRACK_LIST} className='my-auto text-center w-1/4'>Tracks</NavLink>
-                    <div className='my-auto w-1/4'>
-                        <SearchIcon className='h-6 w-6 ml-auto mr-3 border border-gray-400 rounded-md cursor-pointer' />
+            <nav className={`bg-black bg-opacity-90 border-b border-gray-600 ${navCollapsed ? 'h-auto' : 'absolute h-screen'} inset-0 items-center w-full md:flex md:h-auto md:static`}>
+                <div className='flex md:w-1/4'>
+                    <NavLink to={ROUTES.HOME} className='font-display ml-3 text-3xl md:text-2xl'>Kartalytics</NavLink>
+                    <MenuIcon
+                        className='h-8 w-8 md:h-6 md:w-6 my-auto ml-auto mr-3 border border-gray-400 rounded-md cursor-pointer md:hidden'
+                        onClick={() => setNavCollapsed(!navCollapsed)}
+                    />
+                </div>
+
+                <div className={`absolute bg-black bg-opacity-90 ${navCollapsed ? 'invisible opacity-0' : 'visible opacity-100'} items-center mt-9 top-0 transition-all duration-300 w-full z-10 md:flex md:mt-0 md:opacity-100 md:py-1 md:static md:visible md:w-3/4`}>
+                    <NavLink to={ROUTES.PLAYER_LIST} className={linkClasses}>Players</NavLink>
+                    <NavLink to={ROUTES.TOURNAMENT_LIST} className={linkClasses}>Tournaments</NavLink>
+                    <NavLink to={ROUTES.TRACK_LIST} className={linkClasses}>Tracks</NavLink>
+
+                    <div className='text-center w-full md:w-2/5'>
+                        <SearchBar className='w-11/12 my-1 '>
+                        </SearchBar>
                     </div>
                 </div>
             </nav>
---
+++ Kartalytics/ClientApp/src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+﻿import * as React from 'react';
+
+const { useState } = React;
+
+type Props = {
+    children: any;
+    className: string;
+}
+
+const SearchBar: React.FC<Props> = ({ children, className }) => {
+    const [searchText, setSearchText] = useState<string>('');
+
+    return (
+        <>
+            <input
+                className={`mx-1 px-2 py-1.5 rounded text-black ${className}`}
+                type='search'
+                value={searchText}
+                placeholder='Search'
+                aria-label='search'
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchText(e.target.value)}
+            />
+            {children}
+        </>
+    );
+}
+
+export default SearchBar;
