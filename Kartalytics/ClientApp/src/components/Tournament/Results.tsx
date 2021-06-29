7192e163551a1352c3188fe87897e506444f64cd
mdgaydeski <mdgaydeski@gmail.com>
2021-06-28 19:00:32 -0500

    Add tournament results mock (very basic)

--- Kartalytics/ClientApp/src/components/Tournament/Results.tsx
+++ Kartalytics/ClientApp/src/components/Tournament/Results.tsx
@@ -1,9 +1,45 @@
 ﻿import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { PLAYER } from '../../constants/routes';
 
 const Results = () => {
+    const playerList = [
+        { id: 1, name: 'Arlene', place: 1 },
+        { id: 2, name: 'Bret', place: 2 },
+        { id: 3, name: 'Cindy', place: 3 },
+        { id: 4, name: 'Dennis', place: 4 },
+        { id: 5, name: 'Emily', place: 5 },
+        { id: 6, name: 'Frank', place: 6 },
+        { id: 7, name: 'Gert', place: 7 },
+        { id: 8, name: 'Harvey', place: 8 },
+        { id: 9, name: 'Irene', place: 9 },
+        { id: 10, name: 'Jose', place: 10 },
+        { id: 11, name: 'Katrina', place: 11 },
+        { id: 12, name: 'Lee', place: 12 },
+        { id: 13, name: 'Maria', place: 13 },
+        { id: 14, name: 'Nate', place: 14 },
+        { id: 15, name: 'Ophelia', place: 15 },
+        { id: 16, name: 'Philippe', place: 16 },
+        { id: 17, name: 'Rita', place: 17 },
+        { id: 18, name: 'Stan', place: 18 },
+        { id: 19, name: 'Tammy', place: 19 },
+        { id: 20, name: 'Vince', place: 20 },
+        { id: 21, name: 'Wilma', place: 21 }
+    ];
+
     return (
         <>
             <h2>Results</h2>
+            <div>
+                {playerList.map(player => (
+                    <p key={player.id}>
+                        {player.place}.&nbsp;
+                        <Link to={`${PLAYER}/${player.id}`}>
+                            {player.name}
+                        </Link>
+                    </p>
+                ))}
+            </div>
         </>
     );
 }
