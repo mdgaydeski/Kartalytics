1740c913f9cfe60e33583c64c04ba8cefd6df463
mdgaydeski <mdgaydeski@gmail.com>
2021-06-29 06:27:29 -0500

    Add tournament match details mock

--- Kartalytics/ClientApp/src/components/Tournament/Details.tsx
+++ Kartalytics/ClientApp/src/components/Tournament/Details.tsx
@@ -1,9 +1,12 @@
 ﻿import * as React from 'react';
+import MatchTable from '../Data/MatchTable';
 
 const Details = () => {
     return (
         <>
             <h2>Details</h2>
+            <h3>Round 1</h3>
+            <MatchTable />
         </>
     );
 }
