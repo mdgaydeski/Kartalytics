57b8e773c8eafb1c274a05a05994d5e647e5652f
mdgaydeski <mdgaydeski@gmail.com>
2021-07-10 18:36:37 -0500

    Simplify type requirements

--- Kartalytics/ClientApp/src/pages/TournamentList.tsx
+++ Kartalytics/ClientApp/src/pages/TournamentList.tsx
@@ -8,8 +8,8 @@ import { formatDate } from '../utils';
 const { useState, useEffect } = React;
 
 const TournamentList = () => {
-    const [playerList, setPlayerList] = useState<Player[] | null>([]);
-    const [tournamentList, setTournamentList] = useState<Tournament[] | null>([]);
+    const [playerList, setPlayerList] = useState<Player[]>([]);
+    const [tournamentList, setTournamentList] = useState<Tournament[]>([]);
     const [selectedValue, setSelectedValue] = useState<number>(0);
     const [selectedTournament, setSelectedTournament] = useState<Tournament | null>(null);
 
@@ -30,46 +30,42 @@ const TournamentList = () => {
     return (
         <>
             <h1>Tournament List</h1>
-            {
-                tournamentList && <div className='flex items-center mb-4'>
-                    <label htmlFor='tournament'>Select a Tournament: </label>
-                    <select
-                        className='flex-grow mx-2 p-1 rounded text-black'
-                        name='tournament'
-                        id='tournament'
-                        onChange={e => setSelectedValue(Number(e.target.value))}
-                    >
-                        {tournamentList.map(tournament => (
-                            <option value={tournament._id} key={tournament._id}>
-                                {tournament.name}
-                            </option>
-                        ))}
-                    </select>
-                    <AssetLink type='tournament' id={selectedValue}>
-                        <button className='bg-blue-900 px-4 py-1 rounded text-white hover:bg-blue-7000'>
-                            Select
-                    </button>
-                    </AssetLink>
-                </div>
-            }
+            <div className='flex items-center mb-4'>
+                <label htmlFor='tournament'>Select a Tournament: </label>
+                <select
+                    className='flex-grow mx-2 p-1 rounded text-black'
+                    name='tournament'
+                    id='tournament'
+                    onChange={e => setSelectedValue(Number(e.target.value))}
+                >
+                    {tournamentList.map(tournament => (
+                        <option value={tournament._id} key={tournament._id}>
+                            {tournament.name}
+                        </option>
+                    ))}
+                </select>
+                <AssetLink type='tournament' id={selectedValue}>
+                    <button className='bg-blue-900 px-4 py-1 rounded text-white hover:bg-blue-7000'>
+                        Select
+                </button>
+                </AssetLink>
+            </div>
             {
                 selectedTournament && <ul>
                     <li>Location: {selectedTournament.location}</li>
                     <li>Dates: {formatDate(selectedTournament.start_date)} - {formatDate(selectedTournament.end_date)}</li>
-                    {
-                        playerList && <li>Top finishers:
+                    <li>Top finishers:
                         <ol>
-                                {selectedTournament.finalResults.slice(0, 4).map(result => (
-                                    <li className='ml-10' key={result.playerId}>
-                                        {result.place}.&nbsp;
-                                        <AssetLink type='player' id={result.playerId}>
-                                            {playerList.filter(p => p.id === result.playerId)[0].name}
-                                        </AssetLink>
-                                    </li>
-                                ))}
-                            </ol>
-                        </li>
-                    }
+                            {selectedTournament.finalResults.slice(0, 4).map(result => (
+                                <li className='ml-10' key={result.playerId}>
+                                    {result.place}.&nbsp;
+                                    <AssetLink type='player' id={result.playerId}>
+                                        {playerList.filter(p => p.id === result.playerId)[0].name}
+                                    </AssetLink>
+                                </li>
+                            ))}
+                        </ol>
+                    </li>
                 </ul>
             }
         </>
