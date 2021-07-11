61fb7be4d13a7264e32841dd0574c04f0ef6bf5a
mdgaydeski <mdgaydeski@gmail.com>
2021-07-11 07:58:58 -0500

    Delete unused util functions

--- Kartalytics/ClientApp/src/utils.ts
+++ Kartalytics/ClientApp/src/utils.ts
@@ -5,16 +5,3 @@ export const formatDate = (date: string) => {
         ? format(new Date(`${date} 00:00:00`), 'dd MMM yyyy')
         : 'Date unknown'
 }
-
-export const getPlaceTotals = (resultsArray: number[]) => {
-    return resultsArray.reduce((acc, res) => {
-        acc[res - 1]++;
-        return acc;
-    }, [0, 0, 0, 0]);
-}
-
-export const getPoints = (placeTotals: number[]) => {
-    return placeTotals.reduce((acc, place, i) => {
-        return acc + ((3 - i) * place);
-    }, 0);
-}
