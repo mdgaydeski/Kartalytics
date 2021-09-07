fed07a5a3a4ea3f39a04339517022a931286e7d5
mdgaydeski <mdgaydeski@gmail.com>
2021-09-07 10:31:57 -0500

    Deployment prep

--- Kartalytics/Program.cs
+++ Kartalytics/Program.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Kartalytics {
     public class Program {
@@ -16,7 +11,8 @@ namespace Kartalytics {
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
+                    var port = Environment.GetEnvironmentVariable("PORT");
+                    webBuilder.UseStartup<Startup>().UseUrls("http://*:" + port);
                 });
     }
 }
