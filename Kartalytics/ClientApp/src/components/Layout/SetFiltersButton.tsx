b3504f15d9aa79e68ed189301b0328c24f6effcf
mdgaydeski <mdgaydeski@gmail.com>
2021-09-29 19:45:36 -0500

    Apply hover styling

--- Kartalytics/ClientApp/src/components/Layout/SetFiltersButton.tsx
+++ Kartalytics/ClientApp/src/components/Layout/SetFiltersButton.tsx
@@ -8,7 +8,7 @@ type Props = {
 const SetFiltersButton: React.FC<Props> = ({ onClick, cancel }) => {
     return (
         <button
-            className={`${cancel ? 'bg-red-900 md:hidden' : 'bg-green-900'} flex-grow mx-4 px-4 py-1 rounded`}
+            className={`${cancel ? 'bg-red-900 hover:bg-red-700 md:hidden' : 'bg-green-900 hover:bg-green-700'} flex-grow mx-4 px-4 py-1 rounded transition-colors duration-300`}
             onClick={onClick}
         >
             {cancel ? 'Cancel' : 'Apply'}
